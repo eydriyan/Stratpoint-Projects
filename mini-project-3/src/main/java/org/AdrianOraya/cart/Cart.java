b194e5fdba7bea8185e2e6270f01f34d042484a3
eydriyan <115596606+eydriyan@users.noreply.github.com>
2024-06-26 14:37:05 +0800

    added product and cart classes
    added product and cart classes

---
+++ mini-project-3/src/main/java/org/AdrianOraya/cart/Cart.java
@@ -0,0 +1,5 @@
+package org.AdrianOraya.cart;
+
+public class Cart {
+
+}
--- mini-project-3/src/main/java/org/AdrianOraya/cart/Main.java
+++ mini-project-3/src/main/java/org/AdrianOraya/cart/Main.java
@@ -2,6 +2,6 @@ package org.AdrianOraya.cart;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("mini project 3!");
     }
 }
